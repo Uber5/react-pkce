503cf6ab7ffe26f6887946e3f4aca5ccb346e650
Chris <chris@uber5.com>
2019-03-06 05:42:49 +0200

    Added debug ability

--- src/components/auth-context.js
+++ src/components/auth-context.js
@@ -4,19 +4,63 @@ import { getHashValues } from '../lib/utils'
 
 import TokenManager from './token-manager'
 
+const styles = {
+  debug: {
+    position: 'absolute',
+    top: 5,
+    right: 5,
+    background: '#b1d85da8',
+    padding: 10
+  }
+}
+
+class Debug extends React.Component {
+  state = { open: true }
+  render() {
+    const { contextProps, contextState, hashValues } = this.props
+    if (!this.state.open) {
+      return null
+    }
+    return (
+      <div style={ styles.debug }>
+        <button onClick={() => this.setState({ open: false })}>close</button>
+        <ul>
+          <li>contextProps={JSON.stringify(contextProps)}</li>
+          <li>contextState={JSON.stringify(contextState)}</li>
+          <li>hashValues={JSON.stringify(hashValues)}</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
 export class AuthContext extends React.Component {
   getChildContext() {
     const { provider, clientId } = this.props
     return { provider, clientId }
   }
 
+  isDebugEnabled = () => {
+    return !!(localStorage.getItem('debug') || '').match(/react-u5auth/)
+  }
+
   render() {
     const state = getHashValues().state
     const showChildren = !(state && state.match(/^refreshing/))
 
+    const { children, onTokenUpdate, ...contextProps } = this.props
+
+    const debug = this.isDebugEnabled()
+    console.log('react-u5auth, debug', debug)
     return (
       <div>
-        <TokenManager onTokenUpdate={this.props.onTokenUpdate} />
+        <TokenManager
+          onTokenUpdate={token => {
+            this.setState({ token })
+            onTokenUpdate && onTokenUpdate(token)
+          }}
+        />
+        { debug && <Debug contextProps={contextProps} contextState={this.state} hashValues={state} />}
         { showChildren && this.props.children }
       </div>
     )
