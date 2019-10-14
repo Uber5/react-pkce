57e4895c17f01a01a8f7708e017d012f572f82b0
Siya Makhalima <siya@uber5.com>
2019-10-14 14:39:20 +0200

    working on adding the authentication logic, wip

--- src/authenticated.js
+++ src/authenticated.js
@@ -1,16 +1,62 @@
 import React, {useContext, useEffect, useState} from 'react'
-import AuthContext from './auth-context'
+import { authContext as _authContext } from './auth-context'
+import crypto from 'crypto'
+import { getLocalToken } from './local-token'
+import {base64URLEncode, sha256} from './sha256-base64-url-encode'
+
+export function hashed(o) {
+  return Object
+    .getOwnPropertyNames(o)
+    .map(prop => `${ prop }=${ encodeURIComponent(o[prop]) }`)
+    .join('&')
+}
 
 
-export const  authenticated = ({ children }) => {
-  const authContext = useContext(AuthContext)
-  console.log("here is the context ", authContext)
-  sessionStorage.setItem(`context`, authContext)
-  // const [ token, setToken ] = useState(null)
+
+export const  Authenticated = ({ children }) => {
+
+  function authorize(provider, pkce, clientId) {
+    let query = {
+      client_id: clientId,
+      response_type: 'token',
+      redirect_uri: window.location
+    }
+    
+    if(pkce === true) {
+      const code_verifier = base64URLEncode(crypto.randomBytes(32))
+      sessionStorage.setItem("code_verifier",code_verifier)
+      query.code_challenge = base64URLEncode(sha256(code_verifier))
+      query.code_challenge_method = "S256"
+      query.response_type = "code"
+    }
+    console.log("url is ", url)
+    const url = `${ provider }/authorize?${ hashed(query) }`
+    window.location.replace(url)
+  }
+  
+  const authContext = useContext(_authContext)
+  const {clientId, clientSecret, pkce, provider} = authContext
+  console.log('this is the auth context ', authContext)
+  const [ token, setToken ] = useState(null)
+  useEffect(() => {
+    if (!token) {
+      const codeFromUrlHashValues = null // TODO
+      if (codeFromUrlHashValues) {
+        const token = 12345 // TODO: look up token via fetch
+        setToken(token)
+      }
+      // need to authorize
+      const authUrl = provider + "/login" // TODO: create authUrl
+      window.location = authUrl
+    }
+  }, [ ])
+
   if (!token) {
+    // authorize(authContext)
     return <p>Logging in...</p>
   } else {
-    return children({ token : 123456})
+    return children({ token })
   }
 }
+
 
