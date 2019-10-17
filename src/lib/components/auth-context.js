3da054c8b585ff33d7d1e3dc2450b9f812f2aaab
Siya Makhalima <siya@uber5.com>
2019-10-17 11:16:56 +0200

    added validation of the props, WIP

--- src/lib/components/auth-context.js
+++ src/lib/components/auth-context.js
@@ -5,6 +5,10 @@ export const authContext = React.createContext({
 
 const { Provider } = authContext
 export default ({ clientId, pkce, clientSecret, provider, ...props }) => {
+  // TODO: more, and strict, validation
+  if (!(pkce === true || pkce === false)) {
+    throw new Error('invalid pkce value')
+  }
   // ok(clientId, 'clientId is required')
   return <Provider value={{ clientId, pkce, clientSecret, provider}} {...props} />
 }
