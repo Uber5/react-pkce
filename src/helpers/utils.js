025a4ef7a7a15cd5525723e9c49c91c8bfff7f65
Siya Makhalima <siya@uber5.com>
2019-10-17 11:14:46 +0200

    changed to extract the code from the url, hacked needs to be fixed

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,17 +1,21 @@
 
-
+// TODO: HAck
 export const getHashValues = () => {
-  const hash = window.location.hash
-  if (hash.match(/^#/)) {
-    return hash
-      .substring(1)
-      .split('&')
-      .reduce((o, kv) => {
-        const a = kv.split('=');
-        o[a[0]] = a[1];
-        return o;
-      }, {});
-  } else {
-    return {}
-  }
+  const hash = window.location
+  console.log('hashed value ', hash)
+  const splitted = hash.search.split("&")
+  console.log('splitted ', splitted)
+  const code = splitted[0].substring(6)
+  return code
+    // return hash
+    //   .substring(1)
+    //   .split('&')
+    //   .reduce((o, kv) => {
+    //     const a = kv.split('=');
+    //     o[a[0]] = a[1];
+    //     return o;
+    //   }, {});
+  // else {
+  //   return {}
+  // }
 }
