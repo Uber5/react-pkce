b0f25995d2bf3e24f439a95adcb32e9833abcf42
Siya Makhalima <siya@uber5.com>
2019-10-22 13:47:39 +0200

    removed old code and logs

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,22 +1,9 @@
 
-// TODO: HAck
+// TODO: HAck, is it ?
 export const getHashValues = () => {
-  const hash = window.location
-  console.log('hashed value ', hash)
-  const splitted = hash.search.split("&")
-  const redirect_url = hash.origin + hash.pathname
-  console.log('redirect url ', redirect_url)
+  const requestUrl = window.location
+  const splitted = requestUrl.search.split("&")
+  const redirect_url = hash.origin + requestUrl.pathname
   const code = splitted[0].substring(6)
   return {code, redirect_url}
-    // return hash
-    //   .substring(1)
-    //   .split('&')
-    //   .reduce((o, kv) => {
-    //     const a = kv.split('=');
-    //     o[a[0]] = a[1];
-    //     return o;
-    //   }, {});
-  // else {
-  //   return {}
-  // }
 }
